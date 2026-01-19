9640d0fd524850b6b06beed03b5bd02f1d5b6f48
Mario <kuatsimoto@gmail.com>
2026-01-18 23:18:04 -0600

    Add relative line number keybinde Add line buffer to options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,9 +5,15 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.cmd("set number")
 vim.g.diagnostics_active = true
+vim.opt.scrolloff = 20
+
+-- Keymaps
 vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], {noremap = true, silent = true})
 vim.keymap.set('n', '<leader>th', function()
   vim.cmd("split | terminal")
   vim.cmd("wincmd J")
 end, { desc = "Open terminal at at the bottem as split"}
 )
+vim.keymap.set("n", "<leader>r", function()
+  vim.wo.relativenumber = not vim.wo.relativenumber
+end, { desc = "Toggle relative line numbers"})
